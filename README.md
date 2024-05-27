fa8263b43e359099e5b680950e068eaa6fe38ae1
unknown <subiendumaj@gmail.com>
2024-05-27 11:02:20 -0500

    i m subiendu majumdar

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Class test
 This is my First Repo.
 print=("i m raja")
 
-
+print=("i m subiendu)
 
 
---
+++ git-hub/pactis.py
@@ -0,0 +1,80 @@
+##list.. for loop
+family = [45,'subiendu',76,'rajan',67+68,'suvo']
+print('family list of type',family)
+for name in family:
+    print('family',name)
+####list...foor loop
+company= ['wipro premji','ibm arvind','hcl shiva']
+print('company list of type',company)
+for name in company:
+    print('company',name)
+########list for
+owmen=['raja sarkar','jay biswas','suvo roy','3456+8']
+print('owmen list of type',owmen)
+for name in owmen:
+    print('owmen',name)
+######..pactis
+company=['tata # ratan','wipro $ premji ','ibm @ aevind ','hcl & shiva']
+print('company list of type',company)
+for name in company:
+    print('company',name)
+######list for
+address=['name > subiendu','father > khokan ','mother > dipali','sister > soma','brother > somnath','gram > mandalgram','post > man','p.s> memari','pin < 713426']
+print('address list of type',address)
+for name in address:
+    print('address',name)
+#####......
+a=[56+89,'suvo',8*3,'sumon']
+print('a list of type',a)
+for name in a:
+    print('a',name)
+###list..
+c1=['i > am. subiendu','i < am < blive # good']
+print('c1 list of type',c1)
+for name in c1:
+    print('c1',name)
+    c2=['name < subiendu']
+    print('c2 list of type',c2)
+    for name in c2:
+        print('c2',name)
+#########List
+bul=['subho','raja','joy','rajash']
+bool=['rajan','malik']
+print('bul list of type',bul,'bool list of type',bool)
+for name in bul,bool:
+    print('bul',bool,name)
+#########list...
+hr=['jay','subhom','roy','dipok']
+print('hr list of type',hr)
+for name in hr:
+    print('hr',name)
+#####list ...
+deba=['ghosh','sourav','majumdar','biswnath']
+print('deba list of name',deba)
+for name in deba:
+    print('deba',name)
+####list..
+food=['apple','56+4','mango','7*3','berry','8/2']
+print('food list of name',food)
+for name in food:
+    print('food',name)
+####list..
+food=[56+9,'mango',7*3,'apple',90/2,'berry']
+print('food list of name',food)
+for name in food:
+    print('food',name)
+####list
+name=['hello wotrald']
+print('name list of name',name)
+for name in name:
+    print('name',name)
+    print(type(name))
+#####list...
+address=['biswnath',67+90]
+print(address)
+#####python list..
+
+
+
+
+
