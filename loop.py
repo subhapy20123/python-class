a4c3663d016276189acb010c82ba72da5e8b8478
unknown <subiendumaj@gmail.com>
2024-05-30 18:07:34 -0500

    i m raj

--- loop.py
+++ loop.py
@@ -3,3 +3,5 @@ groups=("admin","sels","account","devoloper")
 companies={"admin":"asansol","sels":"ranchi","account":"patna","devoloper":"gaya"}
 for city in address:
     print("city name",city)
+
+print=("raju")
