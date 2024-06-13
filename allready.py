7a7faf74178eed63fe36e2da6662d54c6f0dcd9c
unknown <subiendumaj@gmail.com>
2024-06-13 12:40:35 -0500

    i m subiendu majumdar

---
+++ allready.py
@@ -0,0 +1,13 @@
+def salary (*args,**kwargs):
+    total=10
+    data=kwargs
+    for key,value in data.items():
+        if isinstance(value,str):
+            data*=int(value)
+    else:
+        total*=value
+        return total
+
+result=salary("biswnath","subiendu",raja=9000,raju=9000)
+print("result",result)
+
